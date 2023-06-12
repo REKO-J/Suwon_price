602917bf195786b4442fc3b36965408655cba3f6
YeongHun Jo <117628869+REKO-J@users.noreply.github.com>
2023-06-12 20:17:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ y = [suwon_Jan.loc[suwon_Jan['물가동향'].idxmax()]['물가동향'],
      suwon_May.loc[suwon_May['물가동향'].idxmax()]['물가동향'],]
 
 st.title('수원시 짜장면 지수')
-st.bar(x, y)
+st.bar_chart(x, y)
 st.xlabel('월')
 st.ylabel('(원)', rotation=0, labelpad=5, loc='top')
 st.ylim(6000, 6600)
