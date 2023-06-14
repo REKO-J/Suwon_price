0b07d2fc5f22b3c1a5b531afce4784b75c55e197
YeongHun Jo <117628869+REKO-J@users.noreply.github.com>
2023-06-14 16:18:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ if option == '권선구':
   x = Gwonseon[Gwonseon['품목'] == '짜장면']['물가동향']
   y = Gwonseon[Gwonseon['품목'] == '짜장면']['기준일']
   
-  chart_data = pd.DataFrame(x=x, y=y)
+  chart_data = pd.DataFrame(x, y)
   st.line_chart(chart_data)
 
 elif option == '영통구':
