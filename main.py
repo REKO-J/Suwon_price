9fbf73e4cf24ab1c93aaeb0f0ae9f7746fa22601
YeongHun Jo <117628869+REKO-J@users.noreply.github.com>
2023-06-14 16:01:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ if option == '권선구':
 
   # 짜장면 가격 변동 추이
   x = Gwonseon[Gwonseon['품목'] == '짜장면']['기준일']
-  y = Gwonseon[Gwonseon['품목'] == '짜장면']['물가동향'])
+  y = Gwonseon[Gwonseon['품목'] == '짜장면']['물가동향']
   
   chart_data = pd.DataFrame(x, y)
   st.line_chart(chart_data)
